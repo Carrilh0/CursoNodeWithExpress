217a82de7685c9cf598f9cbe22ca28cdfdf1c689
Vitor Carrilho <vitor.carrilho@hotmail.com>
2020-02-22 12:27:14 -0300

    refatorando rotas

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 var app = require('./config/server');
 
-var rotaNoticias = require('./app/routes/noticias')(app);
-
-var rotaHome = require('./app/routes/home')(app);
-
-var rotaFormInclusaoNoticia = require('./app/routes/form_inclusao_noticia')(app);
+var rotas = require('./app/routes/routes')(app);
 
 app.listen(3000, () => {
     console.log("Servidor criado com sucesso!");
