3c196da99cb57a3df7b19668e77c6cd7fbc68867
Vitor Carrilho <vitor.carrilho@hotmail.com>
2020-03-22 22:53:47 -0300

    noticias na home

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -9,7 +9,12 @@ NoticiasDAO.prototype.getNoticia = function(callback,id){
     this._connection.query(`select * from noticias where id = ${id}`, callback);
 }
 NoticiasDAO.prototype.salvarNoticia = function(noticia, callback){
-    this._connection.query('insert into noticias set ?',noticia, callback);}
+    this._connection.query('insert into noticias set ?',noticia, callback);
+}
+
+NoticiasDAO.prototype.get5UltimasNoticias = function (callback) {
+    this._connection.query('select * from noticias order by created_at desc limit 5',callback);
+}
 
 module.exports = () =>
 {
